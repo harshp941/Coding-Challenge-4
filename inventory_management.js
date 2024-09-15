f7f76c43a2c9ee470f60b4dddf2a842f65f73d8c
Harsh Patel <harshp5@usf.edu>
2024-09-15 16:46:04 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,10 @@
+//Create an Inventory Array of Product Objects
+let inventory = [
+    {name: "Laptop", price: 1400, quanitiy: 12, lowStockLevel:4},
+    {name: "Keyboard", price: 900, quanitiy: 10, lowStockLevel:1}, // These are random items
+    {name: "Mouse", price: 1100, quanitiy: 8, lowStockLevel:3},
+    {name: "Speaker", price: 700, quanitiy: 15, lowStockLevel:5},
+    {name: "TV", price: 1600, quanitiy: 6, lowStockLevel:2},
+];
+console.log(inventory);
+
