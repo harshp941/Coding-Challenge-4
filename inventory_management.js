2580e99d5dcf513578911e952058f9e61a3accb0
Harsh Patel <harshp5@usf.edu>
2024-09-15 21:34:40 -0400

    Create checkLowStock function

--- inventory_management.js
+++ inventory_management.js
@@ -1,16 +1,16 @@
 //Create an Inventory Array of Product Objects
 let inventory = [
-    {name: "Laptop", price: 1400, quanitiy: 12, lowStockLevel:4},
-    {name: "Keyboard", price: 900, quanitiy: 10, lowStockLevel:1}, // These are random items
-    {name: "Mouse", price: 1100, quanitiy: 8, lowStockLevel:3},
-    {name: "Speaker", price: 700, quanitiy: 15, lowStockLevel:5},
-    {name: "TV", price: 1600, quanitiy: 6, lowStockLevel:2},
+    {name: "Laptop", price: 1400, quantity: 12, lowStockLevel:4},
+    {name: "Keyboard", price: 900, quantity: 10, lowStockLevel:1}, // These are random items
+    {name: "Mouse", price: 1100, quantity: 8, lowStockLevel:3},
+    {name: "Speaker", price: 700, quantity: 4, lowStockLevel:5},
+    {name: "TV", price: 1600, quantity: 6, lowStockLevel:2},
 ];
 console.log(inventory);
 
 // Task 2 Create a Function to Display Product Details
 function displayProductDetails(product) {
-const status = product.quanitiy <= product.lowStockLevel ? "Low Stock" : "In Stock";
+const status = product.quantity <= product.lowStockLevel ? "Low Stock" : "In Stock";
 return ` The ${product.name}'s price is $${product.price} and they are ${status}`
 }
 console.log(displayProductDetails(inventory[0]));
@@ -20,12 +20,26 @@ console.log(displayProductDetails(inventory[3]));
 console.log(displayProductDetails(inventory[4]));
 
 // Task 3 Create a Function to Update Product Stock After Sales
-function updateStock(product, unitsSold) {
-    let newStock = product.quantity -= unitsSold
-    return `${product.name} quantity after sales is ${newStock}`
+function updateStock(inventory, unitsSold) {
+    let newStock = inventory.quantity -= unitsSold;
+    return `${inventory.name} quantity after sales is ${newStock}`
 }
 console.log(updateStock(inventory[0], 10));
    
+// Task 4 Create a Function to Check Low Stock Products
+function checkLowStock(product) {
+    product.forEach(product => { 
+    if (product.quantity <= product.lowStockLevel)
+    return console.log(`Product with Low Stock Level: ${product.name}`);}
+    )
+}
+console.log(checkLowStock(inventory));
+
+
+
+
+
+    
 
 
 
